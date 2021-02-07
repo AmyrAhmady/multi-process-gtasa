977bc61a1bf993d547f82db804750c7a56411695
Amyr Ahmady <hhm6@yahoo.com>
2021-02-07 18:20:04 +0330

    use the same variable for old perms in VirtualProtect

--- multi-process-gtasa/main.cpp
+++ multi-process-gtasa/main.cpp
@@ -6,15 +6,19 @@ void EnableMultiProcess()
 		&& !IsBadReadPtr((const void *)0x4028A2, 4)
 		&& *reinterpret_cast<DWORD *>(0x4028A2) == 0xE1FB058D)
 	{
-		DWORD OldProtect, OldProtect2;
+		DWORD OldProtect;
 
+		// Enable windowed mode that can be activated using Enter+Alt (or Alt+Enter for some people)
 		VirtualProtect((LPVOID)0x74872D, 9, 0x40, &OldProtect);
 		*reinterpret_cast<DWORD *>(0x74872D) = 0x90909090;
 		*reinterpret_cast<DWORD *>(0x748731) = 0x90909090;
 		*reinterpret_cast<BYTE *>(0x748735) = 0x90;
 		VirtualProtect((LPVOID)0x74872D, 9, OldProtect, &OldProtect);
 
+		// Disable more than one gtasa instance check so we can run more than one
+		VirtualProtect((LPVOID)0x406946, 4, 0x40, &OldProtect);
 		*reinterpret_cast<DWORD *>(0x406946) = 0;
+		VirtualProtect((LPVOID)0x406946, 4, OldProtect, &OldProtect);
 	}
 }
 
